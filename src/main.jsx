91a00f3a08a4d1b5f24fc0c141f04773657bb13c
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-01-25 15:14:54 +0200

    xz-1-fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/App/App';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
