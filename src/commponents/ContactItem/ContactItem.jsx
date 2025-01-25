763f76735aba24721e49869e2fd5cb940c19e837
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-01-25 15:10:14 +0200

    bag-agan

---
+++ src/commponents/ContactItem/ContactItem.jsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import './ContactItem.module.css';
+
+// const ContactItem = ({ contact }) => {
+//   return (
+//     <li className="ContactItem">
+//       <span>{contact.name}: {contact.number}</span>
+//     </li>
+//   );
+// };
+
+// ContactItem.propTypes = {
+//   contact: PropTypes.shape({
+//     id: PropTypes.string.isRequired,
+//     name: PropTypes.string.isRequired,
+//     number: PropTypes.string.isRequired,
+//   }).isRequired,
+// };
+
+// export default ContactItem;
