fa14c7c5ce7b3aa3bcecaf80fff752afe577eaee
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-01-31 23:24:07 +0200

    add-step-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContactList from './components/ContactList/ContactList.jsx';
 import SearchBox from './components/SearchBox/SearchBox.jsx';
 import ContactForm from './components/ContactForm/ContactForm.jsx'; // Додаємо імпорт
 import './App.module.css';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    // Завантаження контактів з локального сховища під час першого рендерингу
+    const savedContacts = localStorage.getItem('contacts');
+    return savedContacts ? JSON.parse(savedContacts) : [];
+  });
   const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Збереження контактів у локальному сховищі при зміні масиву контактів
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = (newContact) => {
     setContacts([...contacts, newContact]);
